70c7a7019e41633f4bcc02e1e801803ce0609b5c
yaocy <yaocy@glodon.com>
2024-04-12 17:25:44 +0800

    first commit

---
+++ src/main/java/org/day/TestDay1.java
@@ -0,0 +1,37 @@
+package org.day;
+
+/**
+ * @author yaocy
+ * @date 2024-4-12 17:15
+ * @description
+ */
+/**
+ * 1.字符串拼接，比较  String类型
+ * 2.浮点类型的计算，保证精度  BigDecimal
+ * 3.时间格式转换  SimpleDateFormat
+ * 4.集合的排序 去重  ArrayList HashSet
+ * 5.文件读写
+ * 6.uuid的使用
+ * 7.有脏数据,需要考虑边界值
+ */
+
+/**
+ * 1.数据在data.txt下
+ * 2.读取文件,并按以下规则生成新的文件,文件名:data_new.txt
+ * a.输出结果忽略第一行数据
+ * b.name列和nickname 合成一列 举例: 张三(小可爱)
+ * c.salary和subsidy求和合并成一列
+ * d.将time转换成yyyy-MM-dd HH:mm:ss格式
+ * e.每行数据要生成一个唯一值
+ * f.按照age升序排列
+ * g.过滤掉name相同的重复数据,只保留最新的一条
+ * 一点一点百度
+ * 先百度怎么读取文件
+ * 然后是每行的每一列都是逗号分割，再查字符串分割
+ * 然后就是放到一个集合里边对吧 集合里边应该是一个对象
+ */
+public class TestDay1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/day/TestDay2.java
@@ -0,0 +1,23 @@
+package org.day;
+
+/**
+ * @author yaocy
+ * @date 2024-4-12 17:16
+ * @description
+ * */
+/**
+ * //     * 1.读取BaseInfo.txt和 Hobby.txt 两个文件;输出到一个新的文件中,文件名:BaseInfo_Hobby.txt
+ * //     * 2.按照名字去重
+ * //     * 3.按照time排序,time为空的排在最后(现在实现不了todo)
+ * //     * 4.每个人的爱好用 "-"拼接
+ * //     * 5.在倒数第二行输出有相同爱好的人,用","分割,示例: 张三-李四-王五
+ * //     * 6.在最后一行输出爱好里包含球的人,示例: 张三(足球,篮球),李四(排球,棒球)
+ *  命名规范：包名全部小写；类名首字母大写，驼峰命名；变量和方法首字母小写，驼峰命名；全局变量全部字母大写或者下划线拼接
+ * */
+
+
+public class TestDay2 {
+    public static void main(String[] args) {
+
+    }
+}
