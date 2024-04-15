eb7cbde37c7e14198b53b9d0a220b8bd7e5c451f
changhs <changhs@glodon.com>
2024-04-15 16:39:22 +0800

    day3提交

---
+++ src/main/java/org/day3/TestDay3.java
@@ -0,0 +1,27 @@
+package org.day3;
+
+/**
+ * @program: exercises
+ * @description:
+ * @author: hons.chang
+ * @since: 2024-04-15 16:25
+ **/
+
+
+/**
+ * 1. 新增rank.txt表 列说明: 名字 职级 绩效
+ * 2. 读取BaseInfo.txt 和 rank.txt 两个文件;输出到一个新的文件中,文件名:BaseInfo_Rank.txt
+ * 3. 输出结果再BaseInfo的基础上新增一列:年终奖; 年终奖计算规则:  salary*月基数*系数
+ * 月基数：
+ * 职级1:1个月
+ * 职级2:3个月
+ * 职级3:5个月
+ * 系数：
+ * A:2.3
+ * B:1.7
+ * C:0.3
+ * <p>
+ * 考虑边界值
+ */
+public class TestDay3 {
+}
