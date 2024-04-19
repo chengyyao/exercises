81ffc0c5d464b2116d0a725d339ba49c6f6bf392
changhs <changhs@glodon.com>
2024-04-19 17:15:03 +0800

    day5提交

---
+++ src/main/java/org/day5/TestDay5.java
@@ -0,0 +1,22 @@
+package org.day5;
+
+/**
+ * @program: exercises
+ * @description:
+ * @author: yaocy
+ * @since: 2024-04-19 17:02
+ **/
+
+
+/**
+ * 1.新增IncrBaseInfo.json文件  birthday.json文件
+ * 2.需求: 新增入职一批员工,信息在IncrBaseInfo.json文件中;文件中即包含新员工信息也包含老员工信息;
+ * a.将新增员工信息写入到BaseInfo.txt
+ * b.更新老员工的信息,并且写入到BaseInfo.txt
+ * c.增加一列:员工入职天数,并按照入职天数降序排列,入职天数=当前时间-入职时间,写入到BaseInfo.txt
+ * d.找出4月过生日的人,如果没有找到生日信息也当做是4月过生日; 追加到最后行
+ */
+public class TestDay5 {
+
+
+}
